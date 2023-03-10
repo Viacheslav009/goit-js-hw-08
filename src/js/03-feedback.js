b6ba70b92c3ed6bb82c7d9f5657e3e921fc7cb3e
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-10 20:41:19 +0200

    all work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,8 +4,8 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
-dataFromLocalStorage();
+let formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+// dataFromLocalStorage();
 
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
@@ -13,24 +13,29 @@ function onFormData(e) {
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function dataFromLocalStorage() {
+(function dataFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   let email = document.querySelector('.feedback-form input');
   let message = document.querySelector('.feedback-form textarea');
-  console.log(data);
+  // console.log(data);
   if (data) {
-    email.value = data.email;
-    message.value = data.message;
+    email.value = data.email || '';
+    message.value = data.message || '';
   }
-}
+})();
+
 function onSubmitForm(e) {
-  if (form.elements.email.value === '' || form.elements.message.value === '') {
+  if (
+    form.elements.email.value.trim() === '' ||
+    form.elements.message.value.trim() === ''
+  ) {
     return alert('Please fill in all the fields!');
   }
 
-  // console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(formData);
+
   e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
+  formData = {};
 }
